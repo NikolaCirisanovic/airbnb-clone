016a091f579a54c74f3f9fab7d91d86534473c77
NikolaCirisanovic <cirisanovic.nikola@outlook.de>
2019-06-25 09:30:32 +0700

    implementing express, listening to localhost

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
+app.use('/', express.static(path.join(__dirname, 'client')))
+
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
+
+
+app.listen(3000, () => {
+    console.log("Connected to localhost 3000")
+})
