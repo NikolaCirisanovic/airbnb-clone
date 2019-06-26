79d7f8a5f20ab10dff7a189d7086b53d837c877f
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-06-26 14:18:56 +0700

    added db logs

--- db.js
+++ db.js
@@ -3,12 +3,13 @@ const {Client} = require('pg')
 
 // create connection
 const db = new Client({
-	connectionString: 'postgres://postgres:test123@localhost:5432/airbnb'
+	connectionString: process.env.DATABASE_URL
 })
 
 // connect to database
 db.connect((err) => {
 	if (err) {
+		console.log(err)
 		console.log('Error connecting to PostgreSQL database')
 	} else {
 		console.log('Connected to PostgreSQL database')
